ba925158217f66eedabaf66b13379e22eeb53dff
Marc Berger <marc@fourkitchens.com>
2023-10-19 09:05:01 -0700

    feat(YALB-1594): Initial commit of Drupal module as a composer-based module

---
+++ src/Plugin/Block/AIChatbotBlock.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\ys_ai_chatbot\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Block for interfacing with the Yale AI chatbot.
+ *
+ * @Block(
+ *   id = "yale_ai_drupal_chat_block",
+ *   admin_label = @Translation("Yale AI Chatbot"),
+ *   category = @Translation("YaleSites AI Chatbot"),
+ * )
+ */
+class AIChatbotBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    return [
+      '#theme' => 'yale_ai_drupal_chat_block',
+      '#attached' => [
+        'library' => [
+          'yale_ai_drupal_chat/react_app',
+        ],
+      ],
+    ];
+  }
+
+}
